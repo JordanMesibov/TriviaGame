ba1f447796a042b8282266839800b6df1f787919
shinyscyther <mesibovj@gmail.com>
2018-12-21 13:52:08 -0500

    Fixed the Incorrect choices for the first riddle to have IDs instead of classes and fixed the css link in index.html to follow the appropriate flow of folder and file access

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -51,14 +51,14 @@ function gameOver () {
 // create an array of riddles with properties
 var riddles = [{
     riddleText: ["The older I am, the shorter I am. The younger I am, the taller I am. What am I?", "Insert the second riddle here", "Insert the third riddle here"],
-    riddleChoices: ["A Candle", "B: insert the second choice for the first riddle here", "C: insert the first riddles third choice"],
+    riddleChoices: ["A Candle", "A Tree", "A Person"],
     correctChoice: ["A Candle"]
 }];
 
 // declare the function that will display the riddles to the riddleDisplay div
 function riddlesGo () {
   // rewrite the riddleDisplay div to show the first riddle
-  $("#riddleDisplay").html(riddles[0].riddleText[0]);
+  $("#riddleDisplay").html("<h2>" + riddles[0].riddleText[0] + "</h2>");
   // add a line break
   $("#riddleDisplay").append("<br>");
   // append the first option for answering the riddle as a newly created button with an id of correct0 to the riddleDisplay div
@@ -66,21 +66,23 @@ function riddlesGo () {
   // add another line break
   $("#riddleDisplay").append("<br>");
   // append the second option for answering the riddle as a newly created button with a class of incorrect0 to the riddleDisplay div
-  $("#riddleDisplay").append($("<button>" + riddles[0].riddleChoices[1] + "</button>"));
+  $("#riddleDisplay").append($("<button id='incorrect0'>" + riddles[0].riddleChoices[1] + "</button>"));
   // add another line break
   $("#riddleDisplay").append("<br>");
   // append the third option for answering the riddle as a newly created button, also with class of incorrect0 to the riddleDisplay div
-  $("#riddleDisplay").append($("<button>" + riddles[0].riddleChoices[2] + "</button>"));
+  $("#riddleDisplay").append($("<button id='incorrect00'>" + riddles[0].riddleChoices[2] + "</button>"));
   // add event listeners for the right and wrong choices/buttons to trigger the correctScreen and incorrectScreen functions declared globally, afterwards
   document.getElementById("correct0").addEventListener("click", correctScreen);
+  document.getElementById("incorrect0").addEventListener("click", incorrectScreen);
+  document.getElementById("incorrect00").addEventListener("click", incorrectScreen);
 }
 
 // make the function that will show the Correct! screen and display it to the riddleDisplay div
 function correctScreen () {
-  $("#riddleDisplay").html("<h1>Correct!</h3>");
+  $("#riddleDisplay").html("<h1>Correct!</h1>");
 }
 
 // make the function that will show the Incorrect! screen and display it to the riddleDisplay div
 function incorrectScreen () {
-  
+  $("#riddleDisplay").html("<h1>Incorrect!</h1>");
 }
